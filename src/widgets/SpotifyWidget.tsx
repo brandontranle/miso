c4e6a655e276fd1894c596d938b14e8b59e35410
Hannah Truong <truonghannah1@gmail.com>
2023-11-29 19:51:07 -0800

    Added a spotify button and a redirect

--- src/widgets/SpotifyWidget.tsx
+++ src/widgets/SpotifyWidget.tsx
@@ -1,4 +1,15 @@
+import "./SpotifyWidget.css";
+import SpotifyWebApi from 'spotify-web-api-js';
+
 export const SpotifyWidget = ({ handleMinimize, isMinimized }) => {
+  const handleLogin = () => {
+    const clientID = "";
+    const redirectURI = 'http://localhost/';
+    const authEndpoint = 'https://accounts.spotify.com/authorize';
+    const scopes = ['user-read-private', 'user-read-email']; // Add scopes as needed
+    const authUrl = `${authEndpoint}?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scopes.join('%20')}&response_type=token`;
+  };
+
   return (
     <div className="spotify-widget">
       <div className="widget-header">
@@ -23,7 +34,9 @@ export const SpotifyWidget = ({ handleMinimize, isMinimized }) => {
       <>
         <div className="widget-line"></div>
         <div className="widget-content">
-          <p>This is the spotify widget!</p>
+        <button onClick={handleLogin} className="spotify-login-button">
+            Click to login to Spotify
+          </button>
         </div>
       </>
     </div>
