aa0a219bec4b83219a359a4103624b8a07da1355
linivan2003 <linivan2003@gmail.com>
2023-12-06 17:09:43 -0800

    inital commit of GPA calculator

--- src/widgets/MessagesWidget.tsx
+++ src/widgets/MessagesWidget.tsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 export const MessagesWidget = ({ handleMinimize, isMinimized }) => {
+  const [gradeResult, setGradeResult] = useState(""); // State to store the calculated grade
+  const [grades, setGrades] = useState({ grade1: "", grade2: "", grade3: "" });
+  
+  const handleGradeChange = (e) => {
+    const { id, value } = e.target;
+    setGrades((prevGrades) => ({ ...prevGrades, [id]: value }));
+  };
+  const calculateGrade = () => {
+    const { grade1, grade2, grade3 } = grades;
+    const parsedGrade1 = parseFloat(grade1);
+    const parsedGrade2 = parseFloat(grade2);
+    const parsedGrade3 = parseFloat(grade3);
+    const final_grade = (parsedGrade1 + parsedGrade2 + parsedGrade3) / 3;
+    setGradeResult(`Your average GPA is: ${final_grade.toFixed(2)}`);
+  };
   return (
     <div className="catgpt-widget">
       <div className="widget-header">
-        <p className="widget-title">Messages</p>
+        <p className="widget-title">GPA Calculator</p>
         <button className="minimize-symbol" onClick={() => handleMinimize()}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -23,7 +39,19 @@ export const MessagesWidget = ({ handleMinimize, isMinimized }) => {
       <>
         <div className="widget-line"></div>
         <div className="widget-content">
-          <p>This is the chat app widget!</p>
+          <p>
+            <h1>Grade Calculator</h1>
+            <form id="gradeForm">
+              <label htmlFor="grades">Enter GPA: </label>
+              <input type="number" id="grade1" value = {grades.grade1} onChange={handleGradeChange} placeholder="Class 1"></input>
+              <input type="number" id="grade2" value = {grades.grade2} onChange={handleGradeChange} placeholder="Class 2"></input>
+              <input type="number" id="grade3" value = {grades.grade3} onChange={handleGradeChange} placeholder="Class 3"></input>
+              <button type="button" onClick={calculateGrade}>
+                Calculate
+              </button>
+            </form>
+            <p id="result">{gradeResult}</p>
+          </p>
         </div>
       </>
     </div>
