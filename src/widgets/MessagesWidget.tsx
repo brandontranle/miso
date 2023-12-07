8b687fbd2568b62c1931d4f705898a5204436d68
linivan2003 <linivan2003@gmail.com>
2023-12-06 17:26:58 -0800

    Included a Select Grade dropdown, Maps Grades to GPA.

--- src/widgets/MessagesWidget.tsx
+++ src/widgets/MessagesWidget.tsx
@@ -1,20 +1,39 @@
 import { useState } from "react";
+function mapLetterToGPA(letterGrade) {
+  const gradeMap = {
+    'A+': 4.0,
+    A: 4.0,
+    'A-': 3.7,
+    'B+': 3.3,
+    B: 3.0,
+    'B-': 2.7,
+    'C+': 2.3,
+    C: 2.0,
+    'C-': 1.7,
+    'D+': 1.3,
+    D: 1.0,
+    'D-': 0.7,
+    F: 0.0, 
+  };
+
+  return gradeMap[letterGrade.toUpperCase()] || 0;
+}
+
 export const MessagesWidget = ({ handleMinimize, isMinimized }) => {
   const [gradeResult, setGradeResult] = useState(""); // State to store the calculated grade
   const [grades, setGrades] = useState({ grade1: "", grade2: "", grade3: "" });
-  
+
   const handleGradeChange = (e) => {
     const { id, value } = e.target;
     setGrades((prevGrades) => ({ ...prevGrades, [id]: value }));
   };
   const calculateGrade = () => {
     const { grade1, grade2, grade3 } = grades;
-    const parsedGrade1 = parseFloat(grade1);
-    const parsedGrade2 = parseFloat(grade2);
-    const parsedGrade3 = parseFloat(grade3);
-    const final_grade = (parsedGrade1 + parsedGrade2 + parsedGrade3) / 3;
-    setGradeResult(`Your average GPA is: ${final_grade.toFixed(2)}`);
-  };
+    const finalGrade =
+      (mapLetterToGPA(grade1) + mapLetterToGPA(grade2) + mapLetterToGPA(grade3)) / 3;
+    setGradeResult(`Your average GPA is: ${finalGrade}`);
+    };
+
   return (
     <div className="catgpt-widget">
       <div className="widget-header">
@@ -43,9 +62,59 @@ export const MessagesWidget = ({ handleMinimize, isMinimized }) => {
             <h1>Grade Calculator</h1>
             <form id="gradeForm">
               <label htmlFor="grades">Enter GPA: </label>
-              <input type="number" id="grade1" value = {grades.grade1} onChange={handleGradeChange} placeholder="Class 1"></input>
-              <input type="number" id="grade2" value = {grades.grade2} onChange={handleGradeChange} placeholder="Class 2"></input>
-              <input type="number" id="grade3" value = {grades.grade3} onChange={handleGradeChange} placeholder="Class 3"></input>
+              <div>
+              <select id="grade1" value={grades.grade1} onChange={handleGradeChange}>
+                <option value="">Class 1: Select Grade</option>
+                <option value="A+">A+</option>
+                <option value="A">A</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B">B</option>
+                <option value="B-">B-</option>
+                <option value="C+">C+</option>
+                <option value="C+">C</option>
+                <option value="C-">C-</option>
+                <option value="D+">D+</option>
+                <option value="D">D</option>
+                <option value="F">F</option>
+              </select>
+              </div>
+              <div>
+              <select id="grade2" value={grades.grade2} onChange={handleGradeChange}>
+                <option value="">Class 2: Select Grade</option>
+                <option value="A+">A+</option>
+                <option value="A">A</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B">B</option>
+                <option value="B-">B-</option>
+                <option value="C+">C+</option>
+                <option value="C+">C</option>
+                <option value="C-">C-</option>
+                <option value="D+">D+</option>
+                <option value="D">D</option>
+                <option value="F">F</option>
+                {/* Add more grade options */}
+              </select>
+              </div>
+              <div>
+              <select id="grade3" value={grades.grade3} onChange={handleGradeChange}>
+                <option value="">Class 3: Select Grade</option>
+                <option value="A+">A+</option>
+                <option value="A">A</option>
+                <option value="A-">A-</option>
+                <option value="B+">B+</option>
+                <option value="B">B</option>
+                <option value="B-">B-</option>
+                <option value="C+">C+</option>
+                <option value="C+">C</option>
+                <option value="C-">C-</option>
+                <option value="D+">D+</option>
+                <option value="D">D</option>
+                <option value="F">F</option>
+                {/* Add more grade options */}
+              </select>
+              </div>
               <button type="button" onClick={calculateGrade}>
                 Calculate
               </button>
